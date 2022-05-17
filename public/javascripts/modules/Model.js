307e91dc8660f61c60149a36dd0f6b33e14f0a37
Sergio <sp88work@gmail.com>
2022-05-16 20:13:27 -0500

    update model

--- public/javascripts/modules/Model.js
+++ public/javascripts/modules/Model.js
@@ -1,27 +1,13 @@
 "use strict";
 
 import http from './model/Http.js';
-// TODO: import `Store` class (local storage)
 
 export const state = {
   contact: {},
-  favorites: [],
   tags: [],
-  search: {
-    query: '', 
-    contacts: [],
-    contactsCount: 0
-  }
+  contacts: [],
+  contactsCount: 0,
 };
-
-const _formatPhone = (phone) => {
-  let result = "";
-  result += phone.slice(0, 3) + '-';
-  result += phone.slice(3, 6) + '-';
-  result += phone.slice(6, 10);
-  return result; 
-}
-
 
 // Helpers 
 const _formatContact = contact => {
@@ -31,10 +17,19 @@ const _formatContact = contact => {
     id: contact.id, 
     email: contact.email, 
     name: contact.full_name,
-    phone: _formatPhone(contact.phone_number),
+    phone: contact.phone_number,
     tags: contact.tags ? contact.tags.trim().split(',') : []
   }
 }
+
+const _formatPhone = (phone) => {
+  let result = "";
+  result += phone.slice(0, 3) + '-';
+  result += phone.slice(3, 6) + '-';
+  result += phone.slice(6, 10);
+  return result; 
+}
+
 
 
 const _formatSendOffContact = contact => {
@@ -52,7 +47,6 @@ const _formatSendOffContact = contact => {
 
 // Business Logic 
 export const getContacts = async (pattern=null) => {
-  // throw new Error('Ooops, something terrible just happened!');
   try {
     let contactsList = await http.get();   
     contactsList = contactsList.map(_formatContact)
@@ -63,8 +57,8 @@ export const getContacts = async (pattern=null) => {
       })
     } 
     
-    state.search.contacts = contactsList;
-    state.search.contactsCount = state.search.contacts.length;
+    state.contacts = contactsList;
+    state.contactsCount = state.contacts.length;
   } catch (err) {
     throw err; 
   }
@@ -73,14 +67,14 @@ export const getContacts = async (pattern=null) => {
 export const getContactsByTagName = async (tagName) => {
   try {
     await getContacts(); 
-    const contactsList = state.search.contacts.filter(contact => {
+    const contactsList = state.contacts.filter(contact => {
       if (contact.tags.length > 0) {
         return contact.tags.includes(tagName);
       }
     })
 
-    state.search.contacts = contactsList;
-    state.search.contactsCount = state.search.contacts.length;
+    state.contacts = contactsList;
+    state.contactsCount = state.contacts.length;
   } catch (err) {
     throw err; 
   }
@@ -102,7 +96,7 @@ export const createContact = async contact => {
     const sendOffContact = _formatSendOffContact(contact);
     const data = await http.post(sendOffContact); 
     state.contact = _formatContact(data);
-    state.search.contacts.unshift(state.contact);
+    state.contacts.unshift(state.contact);
   } catch (err) {
     throw err; 
   }
@@ -113,8 +107,8 @@ export const deleteContact = async id => {
   try {
     await http.delete(id); 
     
-    if (state.search.contactsCount > 0) {
-      state.search.contacts = state.search.contacts.filter(contact => {
+    if (state.contacts.length > 0) {
+      state.contacts = state.contacts.filter(contact => {
         return contact.id !== id
       });
     } 
@@ -134,11 +128,11 @@ export const updateContact = async contact => {
 
     state.contact = _formatContact(updatedContact); 
 
-    let index = state.search.contacts.findIndex(contact => {
+    let index = state.contacts.findIndex(contact => {
       return contact.id === state.contact.id;
     }); 
 
-    state.search.contacts.splice(index, 1, state.contact);
+    state.contacts.splice(index, 1, state.contact);
 
   } catch (err) {
     throw err; 
@@ -148,10 +142,8 @@ export const updateContact = async contact => {
 
 export const getAllTags = async () => {
   await getContacts(); 
-  let tags = state.search.contacts.flatMap(({ tags }) => tags);
+  let tags = state.contacts.flatMap(({ tags }) => tags);
   state.tags = [...new Set(tags)]; 
-  // persists tags to the correct local storage e.g. Store.save('tags);
-  // update state 
 }
 
 
@@ -160,69 +152,10 @@ export const getAllTags = async () => {
 //   const favorites = new Store('favorites');
 // }
 
-
 // TODO: Implement this when favoriting contact 
 // persist favorites to correct local storage e.g. Store.save('contacts');
-export const getFavoriteContacts = async () => {}
+// export const getFavoriteContacts = async () => {}
 
 
 // TODO: Persist favorites when 
-export const persistFavoriteContacts = async () => {}
-
-
-(async () => {
-  // 1) get all contacts
-  // await getContacts()
-  // console.log('contacts:', state.search.contacts);
-
-
-  // await getContacts('vi')
-  // console.log("contacts with 'vi'", state.search.contacts);
-
-  // 2) get single contact
-  // const contact = await getContact(1)
-  // console.log('Contact By Id:', contact);
-  // console.log(state); 
-
-  // 3) get all tags 
-  // await getAllTags(); 
-  // console.log(state);
-
-  
-  // 4) delete a contact 
-  // await deleteContact(100);
-  // console.log('contacts after deleting:', state.contacts)
-  // console.log('deleted contact:', state.contact)
-
-
-  // 5) create a new contact 
-  // console.log('creating new contact:'); 
-  // const contact = {
-  //   name: "Sergio Pichardo",
-  //   email: "sergio@pichardo.com",
-  //   phone: "12345678901",
-  //   tags: ["work", "friend", "entrepreneur", "software engineer"]
-  // }; 
-  
-  // await createContact(contact);
-  // await createContact(contact);
-  // console.log('\n\n\n')
-  // console.log('New Contact:', state.contact);
-
-  // 6) update a contact 
-  // console.log('before updating a contact:', state.contacts, state.contactsCount)
-  // console.log(state.contact)
-  // const updatedContact = {
-  //   id: 9,
-  //   name: "Sergio J. Pichardo",
-  //   email: "sergiojpichardo@email.com",
-  //   phone: "12345678901",
-  //   tags: ["friend", "entrepreneur", "software engineer"]
-  // }; 
-  
-  // await updateContact(updatedContact)
-  // console.log(state.contact)
-  // console.log('after updating a contact:', state.contacts, state.contactsCount)
-  
-
-})();
+// export const persistFavoriteContacts = async () => {}
